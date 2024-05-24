1c29074a7bdea97c42c070725ed104d0642da75e
Roger0013 <168728024+Roger0013@users.noreply.github.com>
2024-05-24 14:55:10 +0300

    Basic player control

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField] protected float speed = 10.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControl();
+    }
+
+    protected void PlayerControl()
+    {
+        //Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+        //rotate the car instead sliding
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
